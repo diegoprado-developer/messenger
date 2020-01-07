2d754440f6025279408d86c7bafec36675853b98
Prado <diego.prado@capgemini.com>
2020-01-07 16:27:05 -0300

    layout tela de cadastro completa

--- app/src/main/java/com/diegoprado/messenger/LoginActivity.kt
+++ app/src/main/java/com/diegoprado/messenger/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.diegoprado.messenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun loadActivityRegister(view: View){
+        val intent = Intent(applicationContext, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
